2d2cf279385d6a8dcf27f54369b2b1feeadba5f7
PMediaJairo <jairo@profitablemedia.com>
2025-06-13 19:46:01 -0500

    Dividido en diferentes vistas

---
+++ resources/views/index1/footer.blade.php
@@ -0,0 +1,90 @@
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-secondary px-5 mt-5">
+        <div class="row gx-5">
+            <div class="col-lg-8 col-md-6">
+                <div class="row gx-5">
+                    <div class="col-lg-4 col-md-12 pt-5 mb-5">
+                        <h4 class="text-uppercase text-light mb-4">Get In Touch</h4>
+                        <div class="d-flex mb-2">
+                            <i class="bi bi-geo-alt text-primary me-2"></i>
+                            <p class="mb-0">Arizona</p>
+                        </div>
+                        <div class="d-flex mb-2">
+                            <i class="bi bi-envelope-open text-primary me-2"></i>
+                            <p class="mb-0">fitdonewell@gmail.com</p>
+                        </div>
+                        <div class="d-flex mb-2">
+                            <i class="bi bi-telephone text-primary me-2"></i>
+                            <p class="mb-0">+1 954-226-4294</p>
+                        </div>
+                        <div class="d-flex mb-2">
+                            <i class="bi bi-telephone text-primary me-2"></i>
+                            <p class="mb-0">+1 602-527-1055</p>
+                        </div>
+                        <div class="d-flex mt-4">
+                            <a class="btn btn-primary   me-2" href="http://blog.fitdonewell.com/contact">Contact us</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
+                        <h4 class="text-uppercase text-light mb-4 d-none">Quick Links</h4>
+                        <div class="d-flex flex-column justify-content-start d-none">
+                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
+                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
+                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Class Schedule</a>
+                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Our Coaches</a>
+                            <a class="text-secondary mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Latest Blog</a>
+                            <a class="text-secondary" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-5">
+                    <h4 class="text-uppercase text-white mb-4">Free!</h4>
+                    <h6 class="text-uppercase text-white">Subscribe to our</h6>
+                    <p class="text-light"></p>
+                    <form action="">
+                        <div class="input-group">
+                            <a href="http://fitdonewell.com/contact/" class="btn btn-lg btn-dark">Life.Reset.Jourmal.</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid py-4 py-lg-0 px-5" style="background: #111111;">
+        <div class="row gx-5">
+            <div class="col-lg-8">
+                <div class="py-lg-4 text-center">
+                    <p class="text-secondary mb-0">&copy; <a class="text-light fw-bold" href="#">Fit.Done.Well.</a>. All Rights Reserved.</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-dark py-3 fs-4 back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('t1/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('t1/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('t1/lib/counterup/counterup.min.js') }}"></script>
+    <script src="{{ asset('t1/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{ asset('t1/js/main.js') }}?ver=<?php echo rand(1, 10000); ?>"></script>
+
+
+
+</body>
+
+</html>
---
+++ resources/views/index1/header.blade.php
@@ -0,0 +1,45 @@
+    <!-- Header Start -->
+    <div class="container-fluid bg-dark px-0">
+        <div class="row gx-0">
+            <div class="col-lg-3 bg-dark d-none d-lg-block">
+                <a href="" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                    <h1 class="m-0  fit-font">Fit.Done.Well.</h1>
+                </a>
+            </div>
+            <div class="col-lg-9">
+
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0 px-lg-5">
+                    <a href="http://fitdonewell.com/" class="navbar-brand d-block d-lg-none">
+                        <h1 class="m-0   fit-font">Fit.Done.Well.</h1>
+
+                    </a>
+                    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="http://fitdonewell.com" class="nav-item nav-link active">Home</a>
+                            <a href="http://fitdonewell.com#id_about" class="nav-item nav-link">About</a>
+                            <a href="http://shop.fitdonewell.com" class="nav-item nav-link">Shop</a>
+                            <a href="http://fitdonewell.com#id_testimonials" class="nav-item nav-link">Testimonials</a>
+                            <a href="http://fitdonewell.com#id_schedule" class="nav-item nav-link">Schedule</a>
+                            <a href="http://fitdonewell.com#id_events" class="nav-item nav-link">Events</a>
+                            <a href="http://fitdonewell.com#id_coaches" class="nav-item nav-link">Coaches</a>
+
+                            <div class="nav-item dropdown">
+                                <a href="http://blog.fitdonewell.com" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Blog</a>
+                                <div class="dropdown-menu rounded-0 m-0">
+                                    <a href="http://blog.fitdonewell.com" class="dropdown-item">Strong Bodies - Blog</a>
+                                    <a href="http://blog.fitdonewell.com" class="dropdown-item">Open Minds - Blog</a>
+                                    <a href="http://blog.fitdonewell.com" class="dropdown-item">Kind Hearts - Blog</a>
+                                </div>
+                            </div>
+                            <a href="http://blog.fitdonewell.com/contact" class="nav-item nav-link">Contact</a>
+                        </div>
+                        <a href="http://blog.fitdonewell.com/contact" class="btn btn-primary py-md-3 px-md-5 d-none d-lg-block">Join Our Tribe</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->

78921a86f8bbbbb9d7a88e50ff3d4450ddf24179
PMediaJairo <jairo@profitablemedia.com>
2025-08-05 11:51:24 -0500

    Fixed other links

--- resources/views/index1/pillars.blade.php
+++ resources/views/index1/pillars.blade.php
@@ -18,7 +18,7 @@
 
                     <span class="text-justify">At Fit.Done.Well., we believe your body is a powerful tool that deserves celebration. Our body-focused programs are designed to help you build strength, increase endurance, and achieve your fitness goals in a way that honors your unique capabilities. <span class="d-none" id="id_c1">Whether you're engaging in full-body workouts, leg sessions, or core-focused exercises, every movement is a step toward a healthier, stronger you. With personalized routines, live sessions, and expert guidance, we empower you to embrace your body's potential and unlock its true power. Celebrate your health with every workout and transform your body into a source of confidence and vitality.</span></span></p>
                     <a class="text-uppercase" href="javascript:void(0);" OnClick="read_more(1)" id="id_read1">Read More <i class="bi bi-arrow-right"></i></a>
-                    <a href="http://shop.fitdonewell.com" class="btn btn-warning d-block text-center mx-auto mt-4">FIT.RESET.PROGRAMS.</a>
+                    <a href="{{ route('fit-reset-programs') }}" class="btn btn-warning d-block text-center mx-auto mt-4">FIT.RESET.PROGRAMS.</a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6">
@@ -29,7 +29,7 @@
 
                     <span class="text-justify">At Fit.Done.Well., we understand that a strong mind is the foundation of true wellness. Our programs focus on mental clarity, resilience, and mindfulness to help you navigate life's challenges with confidence. <span class="d-none" id="id_c2">By integrating mental exercises, journaling, and mindful habits into your routine, we empower you to stay focused, positive, and centered. Embrace the power of your mind to unlock endless possibilities for personal growth, and discover how mental strength can elevate your overall well-being. With us, your mind becomes a source of strength, peace, and limitless potential.</span></span><br><br></p>
                     <a class="text-uppercase" href="javascript:void(0);" OnClick="read_more(2)" id="id_read2">Read More <i class="bi bi-arrow-right"></i></a>
-                    <a href="http://shop.fitdonewell.com" class="btn btn-warning d-block text-center mx-auto mt-4">MIND.RESET.PROGRAMS.</a>
+                    <a href="{{ route('mind-reset-programs') }}" class="btn btn-warning d-block text-center mx-auto mt-4">MIND.RESET.PROGRAMS.</a>
 
                 </div>
             </div>
@@ -41,7 +41,7 @@
 
                     <span class="text-justify">At Fit.Done.Well., we believe that nurturing your spirit is key to a fulfilling life. Our programs focus on fostering inner peace, mindfulness, and a deeper connection to yourself.<br> <span class="d-none" id="id_c3"> Through mindful activities, meditation, and holistic practices, we guide you on a journey to rediscover balance and purpose. By embracing your spirit, you open the door to greater emotional resilience, compassion, and a sense of fulfillment. Let your spirit soar, and experience the transformative power of a connected and grounded self<br><br></span></span><br> </p>
                     <a class="text-uppercase" href="javascript:void(0);" OnClick="read_more(3)" id="id_read3">Read More <i class="bi bi-arrow-right"></i></a>
-                    <a href="http://shop.fitdonewell.com" class="btn btn-warning d-block text-center mx-auto mt-4">LIFE.RESET.PROGRAMS.</a>
+                    <a href="{{ route('life-reset-programs') }}" class="btn btn-warning d-block text-center mx-auto mt-4">LIFE.RESET.PROGRAMS.</a>
 
                 </div>
             </div>

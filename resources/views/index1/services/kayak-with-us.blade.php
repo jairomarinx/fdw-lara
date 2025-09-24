0a9c705ee5b1193d0e89a7bb04893744986ef9a8
Jairo Marin <jairomarinx@hotmail.com>
2025-09-23 21:38:03 -0500

    Fixed te kayak prices and pother stuff @

--- resources/views/index1/services/kayak-with-us.blade.php
+++ resources/views/index1/services/kayak-with-us.blade.php
@@ -22,7 +22,7 @@
         <div class="row g-5 gb-5">
             <div class="col-lg-4 col-md-6">
                 <div class="bg-light rounded text-justify p-5">
-                    <img src="{{ asset('t1/img/services/kayak3.webp') }}" alt="plan" class="img-fluid">
+                    <img src="{{ asset('river1.webp') }}" alt="plan" class="img-fluid">
                     <h3 class="text-uppercase my-4 fs-1 text-center">River Adventure</h3>
                     <p class="text-center text-uppercase"><strong>Paddle, float, and reconnect with nature</strong><br><br></p>
                     <span class="text-justify">
@@ -37,7 +37,7 @@ Let nature reset your energy.
 
             <div class="col-lg-4 col-md-6">
                 <div class="bg-light rounded text-justify p-5">
-                    <img src="{{ asset('t1/img/services/k3.webp')  }}" class="img-fluid">
+                    <img src="{{ asset('river2.webp')  }}" class="img-fluid">
                     <h3 class="text-uppercase my-4 fs-1 text-center">Scenic Beauty</h3>
                     <p class="text-center text-uppercase"><strong>Wildlife, landscapes, and peaceful movement</strong><br><br></p>
                     <span class="text-justify">
@@ -52,7 +52,7 @@ You will not forget this view or this feeling.
 
             <div class="col-lg-4 col-md-6">
                 <div class="bg-light rounded text-justify p-5">
-                    <img src="{{ asset('t1/img/services/kayak4.webp')  }}" class="img-fluid" alt="">
+                    <img src="{{ asset('river3.webp')  }}" class="img-fluid" alt="">
                     <h3 class="text-uppercase my-4 fs-1 text-center">Breakfast Picnic</h3>
                     <p class="text-center text-uppercase"><strong>Fuel up with good food and great company</strong><br><br></p>
                     <span class="text-justify">
@@ -71,7 +71,9 @@ This is movement with meaning.
       <h2 class="fw-bold mb-4 text-black display-5">Kayak with Us on the Salt River</h2>
       <p class="fs-4 mb-5 text-dark">
         Scenic paddle through desert cliffs and wild beauty<br>
-        <span class="fw-semibold">Only $129. Includes breakfast. Dates will be confirmed.</span>
+        <span class="fw-semibold"> <span style="background-color: rgb(0,230,0); color: white; weight: 600; padding: 10px; "> Only $129.</span> Includes breakfast. </span>
+        <h2 style="color: #28A745; weight: 800; ">Next event: Saturday, October 18 6:00 am to 11:00 am</h2>
+        <h3 class="text-black">Location: Saguaro Lake Guest Ranch 13020 N. Bush Hwy Mesa, AZ 85215</h3>
       </p>
       <a href="{{ route('general-checkout', ['product' => 'kayak-with-us']) }}" class="btn fw-bold" style="background-color: #28a745; color: white; font-size: 1.5rem; padding: 1.4rem 4rem; border-radius: 2rem;">
         Book Now

f618a381f5d52fd14effc399ee14b463f6fa6b9a
Jairo Marin <jairomarinx@hotmail.com>
2025-09-27 10:01:28 -0500

    Cambiado el orden

--- resources/views/index1/home.blade.php
+++ resources/views/index1/home.blade.php
@@ -8,12 +8,14 @@
 @include('index1.gray-social')
 @include('index1.about')
 @include('index1.pillars')
-@include('index1.shop-marquee')
 @include('index1.services-programs-products')
+@include('index1.events')
+@include('index1.schedule')
 @include('index1.estadisticas')
 @include('index1.testimonials')
-@include('index1.schedule')
-@include('index1.events')
+
+@include('index1.shop-marquee')
+
 @include('index1.blog')
 @include('index1.team')
 

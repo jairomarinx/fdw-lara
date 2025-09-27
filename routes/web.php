0df8cd484e6b08499c8bfdcefd41d4df96996702
Jairo Marin <jairomarinx@hotmail.com>
2025-09-26 23:49:38 -0500

    Se cambia el verbo del webhook de github

--- routes/web.php
+++ routes/web.php
@@ -68,5 +68,5 @@ Route::get('/our-programs', fn() => view('programs.main'));
 
 Route::get('/general-checkout/{product}',[CheckoutController::class,'general_checkout'])->name('general-checkout');
 
-Route::get('/admin/deploy',[AdminController::class, "deploy"]);
+Route::post('/admin/deploy',[AdminController::class, "deploy"]);
 

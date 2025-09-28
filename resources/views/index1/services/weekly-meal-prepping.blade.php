d8e39f854786282297ae5d26064661e944ee20ed
Jairo Marin <jairomarinx@hotmail.com>
2025-09-28 10:44:15 -0500

    weekly meal

--- resources/views/index1/services/weekly-meal-prepping.blade.php
+++ resources/views/index1/services/weekly-meal-prepping.blade.php
@@ -31,7 +31,6 @@ Each one crafted with your goals and food preferences in mind.
 No decisions, no hassle—just meals that move you forward.  
 Let your food match your rhythm.
                     </span>
-                    <a href="{{ route('checkout-fit-reset-consultation') }}" class="btn btn-warning d-block text-center mx-auto mt-4">Get Your Custom Plan</a>
                 </div>
             </div>
 
@@ -45,8 +44,8 @@ We prep. You enjoy. Simple as that.
 Your meals are fresh, portioned, and ready when you are.  
 Eat with confidence and consistency without lifting a pan.  
 Support your health with ease, not effort.
-                    </span>
-                    <a href="{{ route('checkout-fit-reset-consultation') }}" class="btn btn-warning d-block text-center mx-auto mt-4">Start Your Reset</a>
+                    </span>                    
+
                 </div>
             </div>
 
@@ -61,7 +60,6 @@ This is structure with freedom. Nourishment with joy.
 Spend less time planning and more time thriving.  
 This is how smart eating becomes second nature.
                     </span>
-                    <a href="{{ route('checkout-fit-reset-consultation') }}" class="btn btn-warning d-block text-center mx-auto mt-4">Make the Shift</a>
                 </div>
             </div>
         </div>
@@ -69,13 +67,16 @@ This is how smart eating becomes second nature.
 
     <div class="text-center" style="background-color: #FDD709; padding: 4rem 2rem; border-radius: 2rem; box-shadow: 0 0 30px rgba(0,0,0,0.2); margin-top: 4rem;">
       <h2 class="fw-bold mb-4 text-black display-5">Choose Your Weekly Meal Plan</h2>
-      <p class="fs-4 mb-5 text-dark">
-        5 meals for $60 &nbsp;|&nbsp; 10 meals for $100 &nbsp;|&nbsp; 15 meals for $120<br>
+      <p class="fs-4 mb-5 text-dark">        
         <span class="fw-semibold">Prepped fresh. Packed with purpose. Ready when you are.</span>
       </p>
-      <a href="/checkout-fit-reset-consultation" class="btn fw-bold" style="background-color: #28a745; color: white; font-size: 1.5rem; padding: 1.4rem 4rem; border-radius: 2rem;">
-        Order Now
+      <a href="{{ url('/general-checkout/weekly-meal-prepping5') }}" class="btn fw-bold" style="background-color: #28a745; color: white; font-size: 1.5rem; padding: 1.4rem 4rem; border-radius: 2rem;">
+        5 Meals 70 Usd
       </a>
+      <a href="{{ url('/general-checkout/weekly-meal-prepping10') }}" class="btn fw-bold" style="background-color: #28a745; color: white; font-size: 1.5rem; padding: 1.4rem 4rem; border-radius: 2rem;">
+        10 Meals 120 Usd
+      </a>
+
     </div>
 
 @endsection

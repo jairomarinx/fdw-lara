05bea6c9bd39247fcb7ccaa14d9780d1be043623
Jairo Marin <jairomarinx@hotmail.com>
2025-12-07 14:56:28 -0500

    Update admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\Presale;
+use Illuminate\Support\Facades\Auth;
+
 
 class AdminController extends Controller
 {
@@ -23,4 +26,20 @@ class AdminController extends Controller
         $output = Artisan::output();
         return response()->json(['Msg'=> 'Deploy Successfull', 'details'=>$output?? 'Error, returned null the artisan command'],200);
     }
+
+    public function dashboard(Request $request)
+    {
+        $user = Auth::user();
+        $email = $user ? $user->email : null;
+
+         $presales = Presale::where('email', $email)
+                ->select('product', 'product_name', 'amount')
+                ->distinct()
+                ->get();
+
+        return view('dashboard', compact('presales'));
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,14 @@ use App\Http\Controllers\AdminController;
 //     return view('welcome');
 // });
 
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard',[AdminController::class,"dashboard"])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
